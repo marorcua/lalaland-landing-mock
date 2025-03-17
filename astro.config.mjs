fbf80ee20f2b726f0498a631a3663c88aec968d5
mario167 <mario@upya.io>
2025-03-17 23:13:05 +0100

    typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+/** @format */
 
-import tailwindcss from '@tailwindcss/vite';
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   }
-});
+})
